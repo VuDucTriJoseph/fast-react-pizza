5e9eeb99cc6ef1b1198c6ff0b21bacd5cb994700
VU DUC TRI JOSEPH <vutrilkvn89@gmail.com>
2025-07-12 11:26:45 +0700

    bild app layout use react router v6, handle feth API, navigation wicth page is loading fetch

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Pizza order React.co</Link>
+      <p>Joseph Vu</p>
+    </header>
+  );
+}
+
+export default Header;
